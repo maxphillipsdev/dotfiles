12ecaf56215a78d14d21dd6c8d725668cdceff46
Max Phillips <max.phillips@nine.com.au>
2024-01-31 12:27:09 +1100

    add tmux-plugin-manager, format some stuff

--- README.md
+++ README.md
@@ -1,23 +1,9 @@
 <img align="center" alt="Cover" src="https://repository-images.githubusercontent.com/577637937/b0ab6312-a079-4638-a093-49e7c6710022">
 
-
-> My personal dotty files
+> Most of my dev environment.
 
 ## Install
 
 ```sh
 ./install
 ```
-
-## Author
-
-👤 **Max Phillips**
-
-* Website: https://maxphillips.dev
-* Twitter: [@maxphillipsdev](https://twitter.com/maxphillipsdev)
-* Github: [@maxphillipsdev](https://github.com/maxphillipsdev)
-* LinkedIn: [@maxphillipsdev](https://linkedin.com/in/maxphillipsdev)
-
-## Show your support
-
-Give a ⭐️ if this project helped you!
--- config/nvim/lua/memp/packer.lua
+++ config/nvim/lua/memp/packer.lua
@@ -24,16 +24,16 @@ return require('packer').startup(function(use)
   use {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x', requires = { --- Uncomment these if you want to manage LSP servers from neovim
-    -- {'williamboman/mason.nvim'},
-    -- {'williamboman/mason-lspconfig.nvim'},
+      -- {'williamboman/mason.nvim'},
+      -- {'williamboman/mason-lspconfig.nvim'},
 
-    -- LSP Support
-    { 'neovim/nvim-lspconfig' },
-    -- Autocompletion
-    { 'hrsh7th/nvim-cmp' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'L3MON4D3/LuaSnip' },
-  }
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'L3MON4D3/LuaSnip' },
+    }
   }
 
   -- nvim-autopairs
